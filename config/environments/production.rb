1e728a475f5ee6ad436bc2c6380fe8b70c31de53
Ryan S <ryan.schneiderman@tufts.edu>
2019-11-25 20:40:07 -0500

    initial commit

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -46,7 +46,7 @@ Rails.application.configure do
   # Mount Action Cable outside main process or domain
   #config.action_cable.mount_path = nil
   #config.action_cable.url = 'wss://ateambball.com/cable'
-  #config.action_cable.allowed_request_origins = [ 'http://ateam1-dev.us-east-1.elasticbeanstalk.com', 'https://ateambball.com' ]
+  #config.action_cable.allowed_request_origins = [ 'http://ateam-dev.us-east-1.elasticbeanstalk.com', 'https://ateambball.com' ]
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
