0779b74e04650e1ba3af6bfd9f6604faea04e9f6
Ryan S <ryan.schneiderman@tufts.edu>
2019-12-13 13:11:20 -0500

    fix free throw rate

--- app/services/advanced/team_offensive_reb_pct_service.rb
+++ app/services/advanced/team_offensive_reb_pct_service.rb
@@ -8,9 +8,9 @@ class Advanced::TeamOffensiveRebPctService
 		if @team_off_reb + @opp_def_reb == 0 
 			raw_oreb_pct = 0.0
 		else
-			raw_oreb_pct = 100 * 1000 *  @team_off_reb / (@team_off_reb + @opp_def_reb)
+			raw_oreb_pct = 100 * 100 *  @team_off_reb / (@team_off_reb + @opp_def_reb)
 		end
-		oreb_pct = raw_oreb_pct.round / 1000.0
+		oreb_pct = raw_oreb_pct.round / 100.0
 		return oreb_pct
 	end
 end
